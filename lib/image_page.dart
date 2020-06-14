3973a20167ccc4388acd8e1930300c8f4ac4f957
Thanigahai vel <37827425+Thanigahai@users.noreply.github.com>
2020-06-14 09:38:39 +0530

    Update image_page.dart
    Used the new "ImagePicker.pickImage" from the image_picker package to select the images.

--- lib/image_page.dart
+++ lib/image_page.dart
@@ -4,6 +4,8 @@ import 'package:image_picker/image_picker.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
+import 'package:uuid/uuid_util.dart';
 
 class ImagePage extends StatefulWidget {
   @override
@@ -12,39 +14,44 @@ class ImagePage extends StatefulWidget {
 
 class _ImagePageState extends State<ImagePage> {
   File _image, cropped,imageFile;
-  final FirebaseStorage _storage =
-  FirebaseStorage(storageBucket: 'gs://image-upload-firebase-9347f.appspot.com');
-  StorageUploadTask _uploadTask;
+  var uuid = new Uuid();
 
-  /// Starts an upload task
-  void _startUpload() {
+  
+  void _startUpload() async{
+    
+    //Create a unique random name for the file to upload
+    var name = uuid.v4(options: {'rng': UuidUtil.cryptoRNG});
+    
+    //Instantiate a path for the files to be uploaded.
+    final ref = FirebaseStorage.instance.ref().child('images/$name.jpg');
+    
+    //We will store the uploaded file's url in this variable to access later
+    var url;
 
-    /// Unique file name for the file
-    String filePath = 'images/${DateTime.now()}.png';
-
-    setState(() {
-      _uploadTask = _storage.ref().child(filePath).putFile(_image);
-    });
+    //As the name says "putfile" is used to put your file (upload) in the firebase
+  await ref.putFile(_image).onComplete.then((value) async{
+    print("Inside then");
+    url = await ref.getDownloadURL();
+    print(url);
+  });
   }
-
-
+  
   Future _selectPicture() async {
-    print("inside function");
     imageFile = await ImagePicker.pickImage(
-        maxHeight: 512, maxWidth: 512, source: ImageSource.gallery);
-    _cropImage();
-    print("3");
+      maxHeight: 512, 
+      maxWidth: 512,
+      source: ImageSource.gallery
+    );
+    imageFile!=null?_cropImage():null;
   }
+  
   Future _takePicture() async {
-    print("inside function");
     imageFile = await ImagePicker.pickImage(
 //        imageQuality: 60,
         maxHeight: 512,
         maxWidth: 512,
         source: ImageSource.camera);
-//    imageFile.path!=null?_cropImage():null;
-    _cropImage();
-    print("3");
+    imageFile!=null?_cropImage():null;
   }
 
   Future<void> _cropImage() async {
@@ -60,17 +67,15 @@ class _ImagePageState extends State<ImagePage> {
         iosUiSettings: IOSUiSettings(
           aspectRatioLockEnabled: true,
         ),
-//      aspectRatioPresets: [CropAspectRatioPreset.ratio5x4],
-        aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+// //      aspectRatioPresets: [CropAspectRatioPreset.ratio5x4],
+        aspectRatio: CropAspectRatio(ratioX: 16, ratioY: 9),
         sourcePath:imageFile.path,
         maxWidth: 512,
         maxHeight: 512);
 
     setState(() {
       _image = cropped_image ?? _image;
-//      CircularProgressIndicator();
       _startUpload();
-
     });
   }
 
@@ -109,7 +114,7 @@ class _ImagePageState extends State<ImagePage> {
 //              fit: BoxFit.cover,
               width: double.infinity,
             )
-          : Text("No image"),
+          : Center(child: Text("No image"),),
     );
   }
 }
